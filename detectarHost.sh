27c2e6c023a5c2b5b4993d71b68267cc6ec19849
EmaBlanco <89885223+emablanco@users.noreply.github.com>
2022-06-10 13:43:46 -0300

    comentario

--- detectarHost.sh
+++ detectarHost.sh
@@ -4,8 +4,8 @@
 #creo un bucle que va del 1 al 254
 for i in $(seq 1 254);do
    
-    #ejecuto el comando y la salida en caso de erro la redirijo a null
-    #en caso de que no produsca erro mando la salida a null
+    #Una vez que ejecuto el binario ping redirijo la salida stdout y stderr la redirijo a /dev/null
+
     timeout 1 ping -c 1 192.168.1.$i 2>/dev/null 1>/dev/null
 
     if [ $? -eq 0 ];then
